470a6b4e970e55461f89af2d3f8eee9141859336
Arian <arian.kulmer@web.de>
2022-02-24 17:52:15 +0100

    vibrator: effect: Read vibration fifo data from vendor
    Change-Id: I58c74f01d75e8616fdb03c27e3dc4c4df6838faa

--- aidl/vibrator/effect/effect.cpp
+++ aidl/vibrator/effect/effect.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020, The Linux Foundation. All rights reserved.
- * Copyright (C) 2022 The LineageOS Project
+ * Copyright (C) 2022-2023 The LineageOS Project
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -34,208 +34,67 @@
 
 #define LOG_TAG "libqtivibratoreffect.xiaomi"
 
-#include <iostream>
+#include <android-base/logging.h>
+#include <filesystem>
 #include <fstream>
-#include <log/log.h>
-#include <sys/stat.h>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 
 #include "effect.h"
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
+namespace {
 
-static const int8_t primitive_0[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-};
+const uint32_t kDefaultPlayRateHz = 24000;
+const uint16_t kPrimitiveMask = (1 << 15);
 
-static const int8_t primitive_1[] = {
-    17,  34,  50,  65,  79,  92,  103, 112, 119, 124,
-    127, 127, 126, 122, 116, 108, 98,  86,  73,  58,
-    42,  26,  9,   -8,  -25, -41, -57, -72, -85, -97,
-    -108, -116, -122, -126, -127, -127, -125, -120,
-    -113, -104, -93,  -80, -66, -51, -35, -18, -1,
-};
+std::unordered_map<uint32_t, effect_stream> sEffectStreams;
+std::unordered_map<uint32_t, std::vector<int8_t>> sEffectFifoData;
 
-static const int8_t primitive_2[] = {
-    17,  34,  50,  65,  79,  92,  103, 112, 119, 124,
-    127, 127, 126, 122, 116, 108, 98,  86,  73,  58,
-    42,  26,  9,   -8,  -25, -41, -57, -72, -85, -97,
-    -108, -116, -122, -126, -127, -127, -125, -120,
-    -113, -104, -93,  -80, -66, -51, -35, -18, -1,
-};
+std::unique_ptr<effect_stream> readEffectStreamFromFile(uint32_t uniqueEffectId) {
+    std::filesystem::path filePath;
 
-static struct effect_stream effects[] = {
-    {
-        .effect_id = 0,
-        .data = 0,
-        .length = 0,
-        .play_rate_hz = 24000,
-    },
-    {
-        .effect_id = 1,
-        .data = 0,
-        .length = 0,
-        .play_rate_hz = 24000,
-    },
-    {
-        .effect_id = 2,
-        .data = 0,
-        .length = 0,
-        .play_rate_hz = 24000,
-    },
-    {
-        .effect_id = 3,
-        .data = 0,
-        .length = 0,
-        .play_rate_hz = 24000,
-    },
-    {
-        .effect_id = 4,
-        .data = 0,
-        .length = 0,
-        .play_rate_hz = 24000,
-    },
-    {
-        .effect_id = 5,
-        .data = 0,
-        .length = 0,
-        .play_rate_hz = 24000,
-    },
-};
+    uint32_t effectId = uniqueEffectId & ~kPrimitiveMask;
 
-static const struct effect_stream primitives[] = {
-    {
-        .effect_id = 0,
-        .data = primitive_0,
-        .length = ARRAY_SIZE(primitive_0),
-        .play_rate_hz = 8000,
-    },
-
-    {
-        .effect_id = 1,
-        .data = primitive_1,
-        .length = ARRAY_SIZE(primitive_1),
-        .play_rate_hz = 8000,
-    },
-
-    {
-        .effect_id = 2,
-        .data = primitive_2,
-        .length = ARRAY_SIZE(primitive_2),
-        .play_rate_hz = 8000,
-    },
-};
-
-// Array containing the paths to fifo data in vendor.
-// The position in the array must match the effect id.
-static const std::string fifo_data_paths[] = {
-    "/vendor/firmware/0_click_P_RTP.bin",
-    "/vendor/firmware/1_doubelClick_P_RTP.bin",
-    "/vendor/firmware/2_tick_P_RTP.bin",
-    "/vendor/firmware/3_thud_P_RTP.bin",
-    "/vendor/firmware/4_pop_P_RTP.bin",
-    "/vendor/firmware/5_heavyClick_P_RTP.bin",
-};
-
-int create_double_click(effect_stream *effect) {
-    const char *path = fifo_data_paths[0].c_str();
-    std::ifstream data;
-    struct stat file_stat;
-    int size = 0;
-    int rc = stat(path, &file_stat);
-
-    if (rc) {
-        ALOGE("Could not open %s", path);
-        return rc;
+    if ((uniqueEffectId & kPrimitiveMask) != 0) {
+        filePath = "/vendor/etc/vibrator/primitive_effect_" + std::to_string(effectId) + ".bin";
     } else {
-        size = file_stat.st_size;
-        // First and last parts of the array contain the click effect, with
-        // blank in between, to stimulate double click
-        effect->length = size * 5;
+        filePath = "/vendor/etc/vibrator/effect_" + std::to_string(effectId) + ".bin";
     }
 
-    // Create a persistent 8-bit int array which contains the fifo data, one
-    // slot of the array contains one byte of the fifo data from vendor.
-    int8_t *custom_data = new int8_t[effect->length];
+    LOG(VERBOSE) << "Reading fifo data for effect " << effectId << " from " << filePath;
 
-    data.open(path, std::ios::in | std::ios::binary);
-    data.read(reinterpret_cast<char *>(custom_data), size);
-    data.close();
-
-    for (int i = 0; i < size; i++) {
-        // Copy the effect onto last part
-        custom_data[size * 4 + i] = custom_data[i];
-        // Blank effect in between
-        custom_data[size + i] = 0;
-        custom_data[size * 2 + i] = 0;
-        custom_data[size * 3 + i] = 0;
+    std::ifstream data(filePath, std::ios::in | std::ios::binary);
+    if (!data.is_open()) {
+        LOG(ERROR) << "Failed to open " << filePath << " for effect " << effectId;
+        return nullptr;
     }
 
-    effect->data = custom_data;
+    std::uint32_t fileSize = std::filesystem::file_size(filePath);
 
-    return rc;
+    std::vector<int8_t> fifoData(fileSize);
+    data.read(reinterpret_cast<char*>(fifoData.data()), fileSize);
+
+    auto result = sEffectFifoData.emplace(uniqueEffectId, std::move(fifoData));
+
+    return std::make_unique<effect_stream>(effectId, fileSize, kDefaultPlayRateHz,
+                                           result.first->second.data());
 }
 
-int parse_custom_data(effect_stream *effect) {
-    const char *path = fifo_data_paths[effect->effect_id].c_str();
-    std::ifstream data;
-    struct stat file_stat;
-    int rc = 0;
+}  // namespace
 
-    ALOGI("Parsing custom fifo data for effect %d from path %s",
-            effect->effect_id, path);
+const struct effect_stream* get_effect_stream(uint32_t effectId) {
+    auto it = sEffectStreams.find(effectId);
+    if (it == sEffectStreams.end()) {
+        std::unique_ptr<effect_stream> newEffectStream = readEffectStreamFromFile(effectId);
 
-    rc = stat(path, &file_stat);
-    if (rc && effect->effect_id != 0) {
-        if (effect->effect_id == 1 /* double click */) {
-            ALOGI("Could not open %s, attempting to create from click", path);
-            return create_double_click(effect);
-        }
-        ALOGI("Could not open %s, falling back to click", path);
-        path = fifo_data_paths[0].c_str();
-        rc = stat(path, &file_stat);
-    }
-    if (!rc) {
-        effect->length = file_stat.st_size;
-    } else {
-        ALOGE("Could not open %s", path);
-        return rc;
-    }
-
-    // Create a persistent 8-bit int array which contains the fifo data, one
-    // slot of the array contains one byte of the fifo data from vendor.
-    int8_t *custom_data = new int8_t[effect->length];
-
-    data.open(path, std::ios::in | std::ios::binary);
-    data.read(reinterpret_cast<char *>(custom_data), effect->length);
-    data.close();
-
-    effect->data = custom_data;
-
-    return rc;
-}
-
-const struct effect_stream *get_effect_stream(uint32_t effect_id)
-{
-    int i;
-
-    if ((effect_id & 0x8000) != 0) {
-        effect_id = effect_id & 0x7fff;
-
-        for (i = 0; i < ARRAY_SIZE(primitives); i++) {
-            if (effect_id == primitives[i].effect_id)
-                return &primitives[i];
+        if (newEffectStream) {
+            auto result = sEffectStreams.emplace(effectId, *newEffectStream);
+            return &result.first->second;
         }
     } else {
-        for (i = 0; i < ARRAY_SIZE(effects); i++) {
-            if (effect_id == effects[i].effect_id) {
-                if (effects[i].length == 0 && parse_custom_data(&effects[i])) {
-                    ALOGE("Could not get custom_data for effect %d", effects[i].effect_id);
-                    return NULL;
-                }
-                return &effects[i];
-            }
-        }
+        return &it->second;
     }
 
-    return NULL;
+    return nullptr;
 }
